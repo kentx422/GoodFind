2a2afdde2b861f2eb7eb41582ea110a75fcf97d8
kento matsui <kmatsui@mikilab.doshisha.ac.jp>
2016-04-08 14:53:53 +0900

    init for Andoroid
    init for Andoroid by kmatsui

---
+++ GoodFindAndroid/app/src/main/java/com/example/isdl/goodfindandroid/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.isdl.goodfindandroid;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
